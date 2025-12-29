b5e954dc6fe32935f75ff87ba6c1d72969aff115
Tyler Barrus <barrust@gmail.com>
2025-12-29 14:23:05 -0500

    Partial hash (#3)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -18,8 +18,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
-    install_requires=[
-    ],
+    install_requires=[],
     entry_points={
         "console_scripts": [
             "deduper=deduper.cli:main",
